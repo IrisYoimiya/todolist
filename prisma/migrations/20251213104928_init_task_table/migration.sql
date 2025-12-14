9084c192a1322d57ce81a4331692e83ca7cc72e4
IrisYoimiya <irischan400@gmail.com>
2025-12-14 12:34:52 +0700

    Add prisma

---
+++ prisma/migrations/20251213104928_init_task_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `tasks` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NULL,
+    `isCompleted` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `deleted_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
